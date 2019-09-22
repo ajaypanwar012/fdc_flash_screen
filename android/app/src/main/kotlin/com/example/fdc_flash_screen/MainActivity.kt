2f0734605dc8f89b3be209aedaada9b7133fe73a
Ajay Panwar <ajaypanwar012@gmail.com>
2019-09-22 14:44:55 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fdc_flash_screen/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fdc_flash_screen
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
